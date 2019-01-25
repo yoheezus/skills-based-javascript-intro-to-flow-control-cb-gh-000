b9553689dac8bbec5d97a1674dbcd0ddae7ee1c2
Yohann Lovell <yoyunyo@hotmail.co.uk>
2019-01-25 22:22:23 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -5,11 +5,22 @@ function basicTeenager(age) {
 }
 
 function teenager(age) {
+    if (age >= 13 && age <= 19) {
+        return "You are a teenager!"
+    } else {
+        return "You are not a teenager"
+    }
 
 }
 
 function ageChecker(age) {
-
+    if (age >= 13 && age <= 19) {
+        return "You are a teenager!"
+    } else if (age <= 12) {
+        return "You are a kid"
+    } else {
+        return "You are a grownup"
+    }
 }
 
 function ternaryTeenager(age) {
