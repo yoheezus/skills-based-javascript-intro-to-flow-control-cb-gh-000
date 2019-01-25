3d0424e76908cfd807c0fc8feb932f7bbfe73ed7
Yohann Lovell <yoyunyo@hotmail.co.uk>
2019-01-25 23:01:02 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -28,5 +28,10 @@ function ternaryTeenager(age) {
 }
 
 function switchAge(age) {
-
+    switch(age) {
+        case (age >= 13 && age <= 19):
+        "You are a teenager"
+        default:
+        "You have an age"
+    }
 }
