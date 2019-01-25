4fff9e0c5b79355d2f10d916d726ae53047bfbd0
Yohann Lovell <yoyunyo@hotmail.co.uk>
2019-01-25 23:07:21 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -1,21 +1,21 @@
 function basicTeenager(age) {
  if (age >= 13 && age <= 19) {
-     return "You are a teenager!"
+     return "You are a teenager!";
  }
 }
 
 function teenager(age) {
     if (age >= 13 && age <= 19) {
-        return "You are a teenager!"
+        return "You are a teenager!";
     } else {
-        return "You are not a teenager"
+        return "You are not a teenager";
     }
 
 }
 
 function ageChecker(age) {
     if (age >= 13 && age <= 19) {
-        return "You are a teenager!"
+        return "You are a teenager!";
     } else if (age <= 12) {
         return "You are a kid"
     } else {
@@ -30,26 +30,13 @@ function ternaryTeenager(age) {
 function switchAge(age) {
     switch(age) {
         case 13:
-            "You are a teenager"
-            break;
         case 14:
-            "You are a teenager"
-            break;
         case 15:
-            "You are a teenager"
-            break;
         case 16:
-            "You are a teenager"
-            break;
         case 17:
-            "You are a teenager"
-            break;
         case 18:
-            "You are a teenager"
-            break;
         case 19:
-            "You are a teenager"
-            break;
+            return "You are a teenager";
         default:
         "You have an age";
     }
